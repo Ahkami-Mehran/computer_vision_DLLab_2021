8ea5564e0a1e447ee5f1f1b30bf9306c4119158e
Ahkami-Mehran <ahkami.mehran@gmail.com>
2021-05-04 12:56:30 +0430

    add miou

--- dt_binary_ss.py
+++ dt_binary_ss.py
@@ -140,9 +140,12 @@ def main(args):
     for epoch in range(15):
         logger.info("Epoch {}".format(epoch))
         # Train
-        t_loss = train(train_loader, model, criterion, optimizer, logger, epoch)
+        t_loss, t_mIoU = train(train_loader, model, criterion, optimizer, logger, epoch)
         writer.add_scalar(
             tag="Training/Mean_loss", scalar_value=t_loss, global_step=epoch
+        )
+        writer.add_scalar(
+            tag="Training/Mean_IoU", scalar_value=t_mIoU, global_step=epoch
         )
         # Validate
         v_loss, v_mIoU = validate(val_loader, model, criterion, logger, epoch)
@@ -170,6 +173,7 @@ def main(args):
 def train(loader, model, criterion, optimizer, logger, epoch):
     # raise NotImplementedError("TODO: training routine")
     loss_meter = AverageValueMeter()
+    iou_meter = AverageValueMeter()
     model.train()
     for i, data in enumerate(loader, 0):
         # len(loader) gives the number of the bataches
@@ -187,20 +191,33 @@ def train(loader, model, criterion, optimizer, logger, epoch):
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        iou_meter.add(mIoU(outputs, labels))
         loss_meter.add(loss.item())
         if DEVICE.type == "cpu":
             if i % 2 == 0:
                 logger.info(
-                    "Training: [epoch:%d, batch: %5d/%d] loss: %.3f"
-                    % (epoch + 1, i + 1, len(loader), loss_meter.mean)
+                    "Training: [epoch:%d, batch: %5d/%d] loss: %.3f , mean_IoU: %.3f"
+                    % (
+                        epoch + 1,
+                        i + 1,
+                        len(loader),
+                        loss_meter.mean,
+                        iou_meter.mean,
+                    )
                 )
         else:
             if i % 100 == 99:
                 logger.info(
-                    "Training: [epoch:%d, batch: %5d/%d] loss: %.3f"
-                    % (epoch + 1, i + 1, len(loader), loss_meter.mean)
+                    "Training: [epoch:%d, batch: %5d/%d] loss: %.3f , mean_IoU: %.3f"
+                    % (
+                        epoch + 1,
+                        i + 1,
+                        len(loader),
+                        loss_meter.mean,
+                        iou_meter.mean,
+                    )
                 )
-    return loss_meter.mean
+    return loss_meter.mean, iou_meter.mean
 
 
 def validate(loader, model, criterion, logger, epoch=0):
